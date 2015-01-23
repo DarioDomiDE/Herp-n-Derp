580d976d99c20e85eaca7da34c58097ec5ba3db3
DarioDomiDE <dario-domi@hotmail.de>
2015-01-23 23:23:05 +0100

    Added UpdateManager, GameEvents and Movement

---
+++ Assets/Scripts/Utility/UpdateManager.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UpdateManager : MonoBehaviour {
+
+    public delegate void InputHandler();
+    public event InputHandler OnUpdate;
+
+	void Update () {
+
+	    if(OnUpdate != null)
+        {
+            OnUpdate();
+        }
+	}
+
+}
