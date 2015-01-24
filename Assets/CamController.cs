afce4f442081e3bfd97b992815cf2a0dd30f4bfe
KallePower <kalle.muenster@gmx.de>
2015-01-24 11:51:24 +0100

    Added Camera

---
+++ Assets/CamController.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CamController : MonoBehaviour 
+{
+	public GameObject players;
+	public GameObject Area;
+	public const float MinimumDistance = 1;
+	public const float MaximumDistance = 7.5f;
+
+	// Use this for initialization
+	void Start() 
+	{
+		players = GameObject.Find("Herb_n_Derb");
+		Area = GameObject.Find("CamArea");
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		Vector3 vec  = (players.transform.position - this.transform.position);
+		float distance = vec.magnitude;
+		this.gameObject.transform.forward = vec;
+		if(distance < MinimumDistance)
+			this.gameObject.transform.position -= ( vec * ( MinimumDistance - distance ) );
+		else if(distance > MaximumDistance)
+			this.gameObject.transform.position += ( vec * ( distance - MaximumDistance ) );
+
+		
+	}
+}
