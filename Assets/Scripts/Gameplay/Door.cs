9a17a15f85eccbad871da4300dfcf23fec940f8c
KallePower <kalle.muenster@gmx.de>
2015-01-24 21:01:27 +0100

    Added Game Manager and whole Ground with Loch

--- Assets/Scripts/Gameplay/Door.cs
+++ Assets/Scripts/Gameplay/Door.cs
@@ -11,20 +11,24 @@ public class Door : MonoBehaviour {
 	
 	}
 	
-	void Update () {
+	//void Update () {
 	
-	}
+	//}
 
     void OnTriggerEnter(Collider other)
     {
         if(other.tag == "Player")
         {
-            if (GameObject.FindGameObjectWithTag("ItemCatcher").GetComponent<ItemCatcher>().CheckItem(this.searchedObject, this.searchedCounter))
+			if(searchedCounter==0)
+			{
+				GameManager.SetNextLevel();
+			}
+            else if (GameObject.FindGameObjectWithTag("ItemCatcher").GetComponent<ItemCatcher>().CheckItem(this.searchedObject, this.searchedCounter))
             {
-                // Do Door
-                GameObject.FindGameObjectWithTag("Timer").GetComponent<Timer>().TransfareToPoints();
-                GameObject.FindGameObjectWithTag("SceneBlender").GetComponent<SceneBlender>().FadeNextScene();
-                SoundManager.Instance.Play("Derp_happy", 1.0f);
+				GameManager.SetNextLevel();
+				//GameObject.FindGameObjectWithTag("Timer").GetComponent<Timer>().TransfareToPoints();
+				//GameObject.FindGameObjectWithTag("SceneBlender").GetComponent<SceneBlender>().FadeNextScene();
+				//SoundManager.Instance.Play("Derp_happy", 1.0f);
             }
         }
 
---
+++ Assets/Scripts/Utility/GameManager.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class GameManager : MonoBehaviour 
+{
+	public static Timer timer;
+	public static SceneBlender blender;
+	public static SoundManager sound;
+	public static Player player;
+	public static Door door;
+	public static List<Item> items;
+
+	// Use this for initialization
+	void Start() 
+	{
+		timer = GameObject.FindGameObjectWithTag("Timer").GetComponent<Timer>();
+		blender = SceneBlender.Instance;
+		sound = SoundManager.Instance;
+		player = GameObject.Find("Player").GetComponent<Player>();
+		door = GameObject.Find("Door").GetComponent<Door>();
+		items = new List<Item>();
+		foreach(Item item in Component.FindObjectsOfType<Item>())
+			items.Add(item);
+
+		if(Points.Instance != null)
+			Points.Instance.ChangeGUI();
+
+	}
+	
+	public static void SetGameOver()
+	{
+		sound.Play("Derp_sad", 1.0f);
+		timer.TransfareToPoints();
+		blender.FadeToScene("gameover");
+	}
+
+	public static void SetGameWin()
+	{
+		sound.Play("Derp_happy", 1.0f);
+		timer.TransfareToPoints();
+		blender.FadeToScene("winscreen");
+	}
+
+	public static void SetNextLevel()
+	{
+		sound.Play("Derp_happy", 1.0f);
+		timer.TransfareToPoints();
+		blender.FadeNextScene();
+	}
+
+
+
+}
