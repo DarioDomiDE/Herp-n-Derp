a0a0d8ff0c3da7b69e7a86adc3df177a1b4fc2fb
KallePower <kalle.muenster@gmx.de>
2015-01-25 01:04:00 +0100

    jelly crate

---
+++ Assets/Scripts/Gameplay/CrateMove.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CrateMove : MonoBehaviour 
+{
+	private Vector3 lastPosition;
+
+	void Start() 
+	{
+		lastPosition = this.gameObject.transform.position;
+	}
+	
+
+	void Update()
+	{
+		if(this.gameObject.transform.position!=lastPosition)
+		{
+			GameManager.sound.Play("Derp - Collision",1.0f);
+			lastPosition = this.gameObject.transform.position;
+		}
+	}
+}
