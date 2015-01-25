722474941aafcffc1554dc3a1923046c4d62d46b
KallePower <kalle.muenster@gmx.de>
2015-01-25 02:16:12 +0100

    swirlytwirly

---
+++ Assets/SwirlyTwirly.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SwirlyTwirly : MonoBehaviour {
+
+	private float angle=0;
+	public float speed = 10;
+	void Start () 
+	{
+	
+	}
+	
+	// Update is called once per frame
+	void Update() 
+	{
+		angle = speed * Time.deltaTime;
+		transform.Rotate(new Vector3(0,0,-1),angle,Space.World);
+	}
+}
