450495fab0f090ab68180bae04e17e2727097887
DarioDomiDE <dario-domi@hotmail.de>
2015-01-25 11:22:05 +0100

    Key fixed

--- Assets/Scripts/Gameplay/TriggerHandler.cs
+++ Assets/Scripts/Gameplay/TriggerHandler.cs
@@ -3,12 +3,6 @@ using System.Collections;
 
 public class TriggerHandler : MonoBehaviour 
 {
-
-	// Use this for initialization
-	void Start ()
-	{
-	
-	}
 	
 	void OnTriggerEnter(Collider other)
 	{
